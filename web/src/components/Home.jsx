31487c0d36a8487b8bfa8980cf5ead97f4d2af06
Dave Brown <dave@moonspider.com>
2017-06-11 21:28:42 -0700

    more flair on home screen

--- web/src/components/Home.jsx
+++ web/src/components/Home.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import FBBlock from './FBBlock.jsx';
 
+class HT extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const base = this.props.base;
+    const to = "/1/" + base;
+    const img = "https://ff.moonspider.com/thumbs/1/" + base + "_240x320_t.jpg";
+    return (
+      <Link to={to}>
+        <img src={img} style={{ width: '120px', height: '160px', margin: '2px' }}/>
+      </Link>
+    );
+  }
+}
+
 export default class Home extends React.Component {
 
   constructor(props) {
@@ -16,24 +33,24 @@ export default class Home extends React.Component {
           <em>TL;DR:</em> Some guy made an app to collect &amp; show weird fruit sculptures for kids.
         </p>
         <div className="flex-container flex-wrap home-thumbs">
-          <Link to="/1/IMG_4075">
-            <img src="https://ff.moonspider.com/thumbs/1/IMG_4075_240x320_t.jpg"/>
-          </Link>
-          <Link to="/1/IMG_3462">
-            <img src="https://ff.moonspider.com/thumbs/1/IMG_3462_240x320_t.jpg"/>
-          </Link>
-          <Link to="/1/IMG_5878">
-            <img src="https://ff.moonspider.com/thumbs/1/IMG_5878_240x320_t.jpg"/>
-          </Link>
-          <Link to="/1/IMG_5194">
-            <img src="https://ff.moonspider.com/thumbs/1/IMG_5194_240x320_t.jpg"/>
-          </Link>
-          <Link to="/1/IMG_4939">
-            <img src="https://ff.moonspider.com/thumbs/1/IMG_4939_240x320_t.jpg"/>
-          </Link>
-          <Link to="/1/IMG_4100">
-            <img src="https://ff.moonspider.com/thumbs/1/IMG_4100_240x320_t.jpg"/>
-          </Link>
+          <HT base="IMG_4075"/>
+          <HT base="IMG_3462"/>
+          <HT base="IMG_5878"/>
+          <HT base="IMG_5194"/>
+          <HT base="IMG_4939"/>
+          <HT base="IMG_4100"/>
+          <HT base="IMG_4169"/>
+          <HT base="IMG_3834"/>
+          <HT base="IMG_4489"/>
+          <HT base="IMG_4214"/>
+          <HT base="IMG_4443"/>
+          <HT base="IMG_5937"/>
+          <HT base="IMG_5893"/>
+          <HT base="IMG_3650"/>
+          <HT base="IMG_2069"/>
+          <HT base="IMG_2121"/>
+          <HT base="IMG_3862"/>
+          <HT base="IMG_3458"/>
         </div>
         <p>
           See the <Link to="/about">about page</Link> for more info.
