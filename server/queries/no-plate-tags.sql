36b6eff148a20602cd617fb005c1f4b0617fc9b8
Dave Brown <dave@moonspider.com>
2017-10-30 10:14:35 -0700

    update debugging query after schema change

--- server/queries/no-plate-tags.sql
+++ server/queries/no-plate-tags.sql
@@ -1,5 +1,6 @@
- select "full_file" from image where base NOT IN (select image_id from image_tag where tag_id in ('blue', 'gray', 'white'));
+select "base" from image where id NOT IN (select image_id from image_tag where tag_id in ('blue', 'gray', 'white'));
 
+-- select image_id from image_tag where tag_id in ('blue', 'gray', 'white');
 -- select "full",coalesce(tag_id,null) from image as im, image_tag as it
 -- where im.base = it.image_id
 -- and (it.tag_id = 'blue' or it.tag_id = 'white' or it.tag_id = 'gray')
